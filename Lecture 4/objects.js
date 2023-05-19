dde3bdbd80fc12906ef89d0a5c741405ae760de7
garimapahwa <104934015+garimapahwa@users.noreply.github.com>
2023-05-19 14:30:19 +0530

    Add files via upload

---
+++ Lecture 4/objects.js
@@ -0,0 +1,16 @@
+// let txt = "";
+// const person = new Object();
+// person.firstname = "garima";
+// person.lastname = "pahwa";
+// person.age = 18;
+// person.eyecolor = "brown";
+// // console.log(person);
+// // const x = person;
+// // x.age = "15";
+// // console.log(x);
+// // console.log(person);
+// for (let x in person){
+//     txt += person[x];
+// }
+// console.log(txt);
+
